4fabad5e10e73a733cb77d3cac355b3cd9670854
nishan <nishanmohammed0097@gmail.com>
2024-11-02 11:06:13 +0530

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+function turnOn() {
+    document.getElementById('lightbulb').src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSij7Nz-LncTbCaf5CnacELAJReMGbFg-XKasJqLSooFohZaDXVgGAIHzwPc92xtRN0m9Q&usqp=CAU';
+}
+
+function turnOff() {
+    document.getElementById('lightbulb').src = 'https://cdn11.bigcommerce.com/s-pvv5u2d/images/stencil/1280x1280/products/12091/47699/4WG45FancyRoundClearGlassWarmWhiteE14LEDBulb__83367.1686031382.jpg?c=2';
+}
